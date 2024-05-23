fc1e0331e6bef95d1c6f67fa8a8a531513cc1efc
Iulian Stefan <stefaniulian95@yahoo.com>
2024-05-23 19:24:02 +0300

    adaugat interfata control robot

---
+++ PetoiBittleControl/app/src/main/java/com/example/petoibittlecontrol/util/BotControlsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.petoibittlecontrol.util
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.petoibittlecontrol.R
+
+class BotControlsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.bot_controls)
+    }
+}
